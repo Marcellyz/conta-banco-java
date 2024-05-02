f14f19b8e71da852afed8b28d4f639036166c889
Marcellyz <marcelly.eduarda4545@gmail.com>
2024-05-01 23:56:09 -0300

    ajustes

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -7,7 +7,7 @@ public class ContaTerminal {
 
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        System.out.println("Digite seu Nome Completo:");
+        System.out.println("Digite seu Nome:");
         String nome = scanner.next();
 
         System.out.println("Digite o numero da sua Agencia com o digito:");
@@ -21,7 +21,7 @@ public class ContaTerminal {
 
         System.out.println("Olá " + nome +",");
         System.out.println("obrigado por criar uma conta em nosso banco, sua agência é " + agencia);        
-        System.out.println("conta " + numero + " e seu saldo R$ " + saldo + " já está disponível para saque");        
+        System.out.println("conta " + numero + " e seu saldo R$ " + saldo + " já está disponível para saque.");        
 
 
     }

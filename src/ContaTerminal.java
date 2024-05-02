e40ea7a13ec5de7e22c4265f936c4b579af86fe8
Marcellyz <marcelly.eduarda4545@gmail.com>
2024-05-01 23:52:37 -0300

    criando a conta terminal

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Seja Bem-vindo ao Banco Terminal!");
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu Nome Completo:");
+        String nome = scanner.next();
+
+        System.out.println("Digite o numero da sua Agencia com o digito:");
+        String agencia = scanner.next();
+
+        System.out.println("Digite o numero da sua conta:");
+        int numero = scanner.nextInt();
+
+        System.out.println("Digite o seu Saldo:");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nome +",");
+        System.out.println("obrigado por criar uma conta em nosso banco, sua agência é " + agencia);        
+        System.out.println("conta " + numero + " e seu saldo R$ " + saldo + " já está disponível para saque");        
+
+
     }
 }
